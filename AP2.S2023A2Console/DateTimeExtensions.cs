0dac442e542e5e1a886751c28495fbafb819320f
wernerdc <manuel.werner3@gmx.de>
2025-09-04 09:51:58 +0200

    implement alternative exercise solution v2

---
+++ AP2.S2023A2Console/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace AP2.S2023A2Console
+{
+    // c# extension methods
+    // used to match syntax of the given AP2 exercise
+    public static class DateTimeExtensions
+    {
+        public static int getDay(this DateTime dateTime)
+        {
+            return dateTime.Day;
+        }
+        
+        public static int getHour(this DateTime dateTime)
+        {
+            return dateTime.Hour;
+        }
+
+        public static int getDaysOfMonth(this DateTime dateTime)
+        {
+            return DateTime.DaysInMonth(dateTime.Year, dateTime.Month);
+        }
+    }
+}
--- AP2.S2023A2Console/Program.cs
+++ AP2.S2023A2Console/Program.cs
@@ -6,25 +6,31 @@ namespace AP2.S2023A2Console
     {
         static void Main(string[] args)
         {
-            List<ComeLeave> entity = LoadComeLeaveDataFromMockJson();
+            List<ComeLeave> entry = LoadComeLeaveDataFromMockJson();
 
-            int[,] visitorsArray = countVisitors(entity);
+            // v1
+            int[,] visitorsArray = countVisitors(entry);
             PrintArrayToConsole(visitorsArray);
+
+            // v2
+            int[,] visitorsArrayV2 = countVisitorsV2(entry);
+            Console.WriteLine("\nVersion 2 \n");
+            PrintArrayToConsole(visitorsArrayV2);
 
             Console.WriteLine("\nPress [ENTER] to exit...");
             Console.ReadLine();
         }
 
-        private static int[,] countVisitors(List<ComeLeave> entity)
+        private static int[,] countVisitors(List<ComeLeave> entry)
         {
-            int days = entity[0].getDate().getDaysOfMonth();
+            int days = entry[0].getDate().getDaysOfMonth();
             int[,] visitors = new int[days, 10];
 
             for (int i = 0; i < days; i++)
             {
                 for (int t = 0; t < 10; t++)
                 {
-                    foreach (ComeLeave e in entity)
+                    foreach (ComeLeave e in entry)
                     {
                         if (e.getDate().getDay() == i + 1 && e.getDate().getHour() == t + 9)
                         {
@@ -43,6 +49,33 @@ namespace AP2.S2023A2Console
                     if (t > 0)
                     {
                         visitors[i, t] += visitors[i, t - 1];
+                    }
+                }
+            }
+
+            return visitors;
+        }
+
+        private static int[,] countVisitorsV2(List<ComeLeave> entry)
+        {
+            int days = entry[0].getDate().getDaysOfMonth();
+            int[,] visitors = new int[days, 10];
+
+            foreach (ComeLeave e in entry)
+            {
+                int row = e.getDate().getDay() - 1;
+                int col = e.getDate().getHour() - 9;
+
+                for (int i = col; i < 10; i++)
+                {
+                    if (e.getComeInOut() == 0)
+                    {
+                        visitors[row, i] += e.getNoPeople();
+                    }
+                    else
+                    {
+                        if (i < 9) 
+                            visitors[row, i + 1] -= e.getNoPeople();
                     }
                 }
             }
@@ -79,27 +112,6 @@ namespace AP2.S2023A2Console
                     Console.WriteLine("--------------------------------------------------------------------");
                 }
             }
-        }
-    }
-
-
-    // c# extension methods
-    // used to match syntax of the given AP2 exercise
-    public static class DateExtensions
-    {
-        public static int getDay(this DateTime dateTime)
-        {
-            return dateTime.Day;
-        }
-        
-        public static int getHour(this DateTime dateTime)
-        {
-            return dateTime.Hour;
-        }
-
-        public static int getDaysOfMonth(this DateTime dateTime)
-        {
-            return DateTime.DaysInMonth(dateTime.Year, dateTime.Month);
         }
     }
 }
